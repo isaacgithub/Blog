2cef8c5128581471828a2d77fb6950e5ef9760ff
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2013-09-19 01:11:27 -0300

    Kinect posts
    Posts do blog que possuem ligação com o Kinect

---
+++ Kinect/KinectProjects/KinectConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KinectUtil.KinectInitializer initializer = new KinectUtil.KinectInitializer();
+            initializer.Initialize(
+                (kinect) =>
+                {
+                    kinect.ElevationAngle = 10;
+                }
+                );
+        }
+
+    }
+}
