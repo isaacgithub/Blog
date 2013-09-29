d0b6b58ef6f9600f44c72bec0429022913c89616
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2013-09-29 18:44:50 -0300

    Camera RGB em Aplicação WPF

---
+++ Kinect/CameraRGB/KinectProjects/KinectApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private WriteableBitmap _imageSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeKinect();
+        }
+
+        private void InitializeKinect()
+        {
+            KinectUtil.KinectInitializer initializer = new KinectUtil.KinectInitializer();
+            initializer.Initialize(
+                (kinectSensor) =>
+                {
+                    kinectSensor.ColorStream.Enable();
+                    kinectSensor.ColorFrameReady += ColorFrameReady;
+                });
+
+            chooserUI.KinectSensorChooser = initializer.KinectChooser;
+        }
+
+        private void ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame currentFrame = e.OpenColorImageFrame())
+            {
+                if (currentFrame != null)
+                {
+                    byte[] imageBytes = new byte[currentFrame.PixelDataLength];
+                    currentFrame.CopyPixelDataTo(imageBytes);
+
+                    if (_imageSource == null)
+                    {
+                        _imageSource = new WriteableBitmap(BitmapImage.Create(currentFrame.Width, currentFrame.Height, 96, 96,
+                                                                              PixelFormats.Bgr32, null, imageBytes,
+                                                                              currentFrame.Width * currentFrame.BytesPerPixel));
+                        KinectCameraImage.Source = _imageSource;
+                    }
+                    else
+                        _imageSource.WritePixels(new Int32Rect(0, 0, currentFrame.Width, currentFrame.Height), imageBytes, currentFrame.Width * currentFrame.BytesPerPixel, 0);
+                }
+            }
+        }
+    }
+}
---
+++ Kinect/CameraRGB/KinectProjects/KinectUtil/KinectInitializer.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectUtil
+{
+    public class KinectInitializer
+    {
+        private KinectSensorChooser _kinectChooser;
+        private Action<KinectSensor> _initializeResourcesMethod;
+
+        public KinectSensor Kinect
+        { get { return _kinectChooser.Kinect; } }
+
+        public KinectSensorChooser KinectChooser
+        { get { return _kinectChooser; } }
+
+        public void Initialize(Action<KinectSensor> initializeResourcesMethod)
+        {
+            _initializeResourcesMethod = initializeResourcesMethod;
+
+            _kinectChooser = new KinectSensorChooser();
+            _kinectChooser.KinectChanged += KinectChooser_KinectChanged;
+            _kinectChooser.Start();
+        }
+
+        private void KinectChooser_KinectChanged(object sender, KinectChangedEventArgs e)
+        {
+            KinectSensor oldSensor = e.OldSensor;
+            if (oldSensor != null)
+            {
+                try
+                {
+                    if (oldSensor.ColorStream.IsEnabled)
+                        oldSensor.ColorStream.Disable();
+
+                    if (oldSensor.DepthStream.IsEnabled)
+                        oldSensor.DepthStream.Disable();
+
+                    if (oldSensor.SkeletonStream.IsEnabled)
+                        oldSensor.SkeletonStream.Disable();
+
+                }
+                catch (InvalidOperationException ex)
+                {
+                    //Caso o sensor entre em um estado inválido ele cairá nesta exceção
+                }
+            }
+
+            if (e.NewSensor != null)
+            {
+                if (_initializeResourcesMethod != null)
+                    _initializeResourcesMethod(e.NewSensor);
+            }
+        }
+
+    }
+}
