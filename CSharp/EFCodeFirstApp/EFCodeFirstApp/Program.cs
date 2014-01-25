fc98382af27993aaa0b87cd80b4a4ecb556ca9d8
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2014-01-24 23:15:44 -0200

    Commit EF

---
+++ CSharp/EFCodeFirstApp/EFCodeFirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inserção
+            //...
+            //Vehicle vehicle = new Vehicle();
+            //vehicle.Year = 2000;
+            //vehicle.Model = "Honda";
+
+            //DataBase db = new DataBase();
+            //db.Vehicle.Add(vehicle);
+            //db.SaveChanges();
+            //...
+
+            //Leitura
+            //...
+            //DataBase db = new DataBase();
+            //foreach (var vehicle in db.Vehicle.Where(v => v.Year > 100))
+            //{
+            //    Console.WriteLine(vehicle.Id);
+            //}
+            //Console.ReadKey();
+            //...
+        }
+    }
+}
